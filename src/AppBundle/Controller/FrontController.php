8e82097f7edab9756f657a1179831bd82002223d
pellecuer <david-d.pellecuer@edf.fr>
2018-10-11 13:59:22 +0200

    maj front

--- src/AppBundle/Controller/FrontController.php
+++ src/AppBundle/Controller/FrontController.php
@@ -9,10 +9,68 @@ use Symfony\Component\Routing\Annotation\Route;
 class FrontController extends Controller
 {
     /**
-     * @Route("/see", name="seePlanning")
+     * @Route("/", name="home")
      */
     public function indexAction(Request $request)
     {       
+        return $this->render('default/index.html.twig');
+    }
+    
+    
+    /**
+     * @Route("/seeplanning", name="seeplanning")
+     */
+    public function seePlanningAction(Request $request)
+    {       
         return $this->render('default/seePlanning.html.twig');
+    }
+    
+    /**
+     * @Route("/doc", name="doc")
+     */
+    public function docAction(Request $request)
+    {       
+        return $this->render('default/doc.html.twig');
+    }
+    
+    
+    /**
+     * @Route("/codes", name="codes")
+     */
+    public function codesAction(Request $request)
+    {       
+        return $this->render('default/codes.html.twig');
+    }
+    
+    /**
+     * @Route("/reglementation", name="reglementation")
+     */
+    public function reglementationAction(Request $request)
+    {       
+        return $this->render('default/reglementation.html.twig');
+    }
+    
+    /**
+     * @Route("/createdatetranche", name="createdatetranche")
+     */
+    public function createDateTrancheAction(Request $request)
+    {       
+        return $this->render('default/createDateTranche.html.twig');
+    }
+    
+    /**
+     * @Route("/accueiladmin", name="accueiladmin")
+     */
+    public function accueilAdminAction(Request $request)
+    {       
+        return $this->render('default/accueilAdmin.html.twig');
+    }
+    
+    /**
+     * @Route("/droitsroles", name="droitsroles")
+     */
+    public function droitsRolesAction(Request $request)
+    {       
+        return $this->render('default/droitsRoles.html.twig');
     }
 }
