1d5ac416434a95271d90234da82077e10b677fc0
pellecuer <david-d.pellecuer@edf.fr>
2018-11-07 09:18:20 +0100

    ok arrays

--- src/AppBundle/Controller/AgendaController.php
+++ src/AppBundle/Controller/AgendaController.php
@@ -83,120 +83,37 @@ class AgendaController extends Controller {
             //dump($startDate);die;
 
         }else {
-            $startDate = new \DateTimeImmutable('now - 20 days',  new \DateTimeZone('Europe/Paris'));
+            $startDate = new \DateTime('now - 200 days',  new \DateTimeZone('Europe/Paris'));
             $endDate = new \DateTime(('11-01-2019'));
             //dump($endDate);die;
-        } 
-        
-        //build arrayDate
-        $startDateArray = $startDate;
-        $endDateArray = $endDate;
-        $diff=$startDateArray->diff($endDateArray)->days;
-        $diff1Day = new \DateInterval('P1D');
-        $arrayDate = [];
-        for ($i=0;$i<$diff;$i++){
-            $arrayDate[] =  $startDateArray;
-             $startDateArray = $startDateArray->add($diff1Day);
         }
-        //dump($arrayDate);die;
-         
-
-        $agent = ['Durant', 'Dupont', 'Michel'];        
-
-        for ($i = 0; $i < count($agent); $i++) {            
+        
+        //build letterArray
+        $agent = ['Dupont', 'Durant', 'Michel'];
+        $agentBetweens = [];
+        For ($i=0; $i<count($agent); $i++){
             $agentBetweens[] = $this->getDoctrine()
                 ->getRepository(Agenda::class)
                 ->findAgentBetweenDate($startDate, $endDate, $agent[$i]);
-         }
-
+        }
+        
+        //build dateArray        
+        $diff=$startDate->diff($endDate)->format("%a");
+        $interval = new \DateInterval('P1D');
+        $arrayDate = [];
+        $immutable = \DateTimeImmutable::createFromMutable($startDate);
+        while ($immutable<$endDate){
+            $arrayDate[] =  $immutable;
+             $immutable = $immutable->add($interval);
+        }
+        //dump($arrayDate);die;
          
+   
         return $this->render('agenda.html.twig', [
 
        'dateBetweens' => $arrayDate,
         'agentBetweens' => $agentBetweens,
          'form'=>$form->createView()                
         ]);
-        }   
-        
-        
-        /*$form = $this->createForm(AgendaType::class);
-        $form->handleRequest($request);
-       
-         if ($form->isSubmitted() && $form->isValid()) {            
-            $data = $form->getData();            
-            $startDate = $data['$startDate'];            
-            $endDate = $data['$endDate'];
-            
-            return $this->redirectToRoute('filterByDate', array(
-                'startDate' => $startDate,
-                'endDate' => $endDate,
-            ));
-         }
-         else {
-         
-         */
-             
-       
-         
-
-              
-    
-    
-    /**
-     *  Lists all agenda entities.
-     *
-     * @Route("/agenda/{startDate}/{endDate}}", name="filterByDate")
-     * @Method({"GET", "POST"})
-     */
-     public function filterByDateAction(Request $request, \DateTime $startDate, \DateTime $endDate)
-    {
-        $form = $this->createForm(AgendaType::class);
-        $form->handleRequest($request);               
-        
-        //récupérer les placeholder
-        
-        dump($startDate);
-         
-        //Créer le formulaire
-        //setter la value des dates et team
-        //Générer le tableau des dates
-        // Générer le tableau des lettres
-        // Envoyer la vue
-         
-     }
-     
-     
-     /**
-     *  Lists all agenda entities.
-     *
-     * @Route("/test", name="test")
-     * @Method({"GET", "POST"})
-     */
-     public function testAction(Request $request)
-    {
-        $defaultData = array('message' => 'Type your message here');
-        $form2 = $this->createFormBuilder($defaultData)
-            ->add('name', TextType::class)
-            ->add('email', EmailType::class)
-            ->add('message', TextareaType::class)
-            ->add('send', SubmitType::class)
-            ->getForm();
-        
-
-        $form2->handleRequest($request);
-
-        if ($form2->isSubmitted() && $form2->isValid()) {
-            // data is an array with "name", "email", and "message" keys
-            $data = $form2->getData();
-           dump($data);die;
-        }
-         
-        
-        return $this->render('test.html.twig', [
-
-       
-         'form2'=>$form2->createView()
-        ]);           
-     }
-     
+        } 
 }
