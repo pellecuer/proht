e02cab37e39172373fdf602554d0e284bf4831b5
pellecuer <david-d.pellecuer@edf.fr>
2018-12-06 10:53:08 +0100

    maj

---
+++ src/AppBundle/Repository/UserRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace AppBundle\Repository;
+
+/**
+ * Description of UserRepository
+ *
+ * @author E32979
+ */
+class UserRepository extends \Doctrine\ORM\EntityRepository
+{
+    //put your code here
+}
+
