75e8d5e8e2a8798f0ea0189a7f0fa0a3a6662d3d
pellecuer <david-d.pellecuer@edf.fr>
2019-02-06 11:35:48 +0100

    maj

--- src/AppBundle/Controller/AgentController.php
+++ src/AppBundle/Controller/AgentController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 
 /**
@@ -75,12 +76,10 @@ class AgentController extends Controller
     
     /**
      * @Route("/show", name="showagent")
+     * @Security("is_granted('ROLE_ADMIN')", statusCode=404, message="Vous ne disposez pas de droits suffisants pour visualiser les agents")
      */
     public function showAction()
     {
-             // finds *all* products 
-        $user = $this->getUser();
-        
         $agents = $this->getDoctrine()
                 ->getRepository(Agent::class)
                 ->findAll();
